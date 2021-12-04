f06d9bdedeb9496522100ca4e92536b6aaa3d062
Duley Williams <duleygrafix@gmail.com>
2021-12-04 10:15:31 -0600

    totalfinal

--- scripts/main.js
+++ scripts/main.js
@@ -15,24 +15,6 @@ document.addEventListener("stateChanged", event => {
     renderAllHTML()
 })
 
-const metals = getMetals()
-
-// Remember that the function you pass to find() must return true/false
-const foundMetal = metals.find(
-    (metal) => {
-        return metal.id === order.metalId
-    }
-)
-const totalCost = foundMetal.price
-
-const costString = totalCost.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD"
-})
-
-`<li>
-    Order #${order.id} cost ${costString}
-</li>`
 
 
 
